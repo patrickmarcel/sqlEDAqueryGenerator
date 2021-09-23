36173433b3df9c6c55d436bbaa5494dfe04007d0
Alexandre Chanson <a.chanson.tours@gmail.com>
2021-09-23 17:46:09 +0200

    switching machines

--- src/main/java/fr/univtours/info/dataset/metadata/DatasetStats.java
+++ src/main/java/fr/univtours/info/dataset/metadata/DatasetStats.java
@@ -16,25 +16,40 @@ public class DatasetStats {
     //size of active domain of dimensions
     @Getter
     HashMap<DatasetDimension, Integer> adSize;
+    //width (for strings)
+    HashMap<DatasetDimension, Integer> avgWidth;
     //absolute frequency of values of dimensions
     @Getter
     HashMap<DatasetDimension, HashMap<String, Integer>> frequency;
     @Getter
     int rows;
+    private List<DatasetDimension> dimensions;
+    private List<DatasetMeasure> measures;
 
     public DatasetStats(DBConfig config) throws SQLException {
 
         table = config.getTable();
-        List<DatasetDimension> theDimensions;
-        theDimensions = config.getDimensions();
+        dimensions = config.getDimensions();
+        measures = config.getMeasures();
         Connection conn = config.getConnection();
 
         // Pre compute stats
         adSize = new HashMap<>();
         frequency = new HashMap<>();
+        avgWidth = new HashMap<>();
+
+        // strings
+        for (DatasetDimension dim : dimensions) {
+            String sql = "select avg_width from pg_stats where tablename = '" + table + "' and attname = '"+dim.getPrettyName()+"';";
+            Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = st.executeQuery(sql) ;
+            rs.next();
+            avgWidth.put(dim, rs.getInt(1));
+            st.close();
+        }
 
         // Active domain size
-        for (DatasetDimension dim : theDimensions) {
+        for (DatasetDimension dim : dimensions) {
             String sql = "select count(distinct " + dim.getName() + ") from " + table + ";";
             Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
             ResultSet rs = st.executeQuery(sql) ;
@@ -43,7 +58,7 @@ public class DatasetStats {
             st.close();
         }
         // Absolute frequency
-        for (DatasetDimension dim : theDimensions) {
+        for (DatasetDimension dim : dimensions) {
             HashMap<String, Integer> tmp = new HashMap<>();
             String sql = "select " + dim.getName() + ", count(*) from " + table + " group by " + dim.getName() + ";";
             Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
@@ -63,5 +78,10 @@ public class DatasetStats {
         st.close();
 
         //conn.close();
+    }
+
+    public long estimateAggregateSize(List<DatasetDimension> groupBy){
+        int len = groupBy.stream().mapToInt(d -> adSize.get(d)).reduce(Math::multiplyExact).getAsInt();
+        return 64L *measures.size()*len + dimensions.stream().mapToInt(d -> 64*len*avgWidth.get(d)).sum();
     }
 }
